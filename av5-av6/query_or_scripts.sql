a22cc69c59741591c2bdf49252ee858f2f76ae38
Pedro Baptista <pedrolk2012@gmail.com>
2025-03-27 22:53:48 -0300

    feat: create companhia area queries

--- av5-av6/query_or_scripts.sql
+++ av5-av6/query_or_scripts.sql
@@ -1,5 +1,14 @@
 ---- ()
 
-SELECT a.codigo_aeronave, a.modelo, REF(c) AS companhia_ref
+SELECT a.codigo_aeronave, a.modelo, DEREF(REF(c)).razao_social as Razao_social
 FROM tb_aeronaves a, tb_companhias_aereas c
 WHERE a.companhia_aerea = REF(c);
+
+
+SELECT a.codigo_aeronave, a.modelo, DEREF(REF(comp)).razao_social as Razao_social
+FROM tb_aeronaves a, tb_companhias_aereas comp
+WHERE a.companhia_aerea  IN (
+    SELECT REF(c) 
+    FROM tb_companhias_aereas c
+    WHERE c.quantidade_total_de_funcionarios > 3500
+);
