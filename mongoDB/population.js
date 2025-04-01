1710d375cf9ab9cf954f6d7c92d9dfbe4baca5c6
Breno <brenomelogabriel2019@gmail.com>
2025-03-31 22:57:00 -0300

    feat(population): adding more enterprises

--- mongoDB/population.js
+++ mongoDB/population.js
@@ -5,7 +5,7 @@ db.createCollection('vagas');
 db.createCollection('processos_seletivos');
 db.createCollection('candidatos');
 
-db.empresas.insertMany(
+db.empresas.insertMany([
 
     {
 
@@ -37,7 +37,7 @@ db.empresas.insertMany(
             "rua": "Travessa do Normando",
             "bairro": "Pina", 
             "cidade":"Recife", 
-            "estado": 'pernambuco', 
+            "estado": 'Pernambuco', 
             'uf': 'PE', 
             "cep": "51011635"
 
@@ -75,7 +75,7 @@ db.empresas.insertMany(
             "rua": "Avenida central",
             "bairro": "Areias", 
             "cidade":"Recife", 
-            "estado": 'Recife', 
+            "estado": 'Pernanbuco', 
             'uf': 'PE', 
             "cep": "50781510"
 
@@ -94,7 +94,7 @@ db.empresas.insertMany(
             "rua": "Rua Subida do Curupá",
             "bairro": "Nova Descoberta", 
             "cidade":"Recife", 
-            "estado": 'Recife', 
+            "estado": 'Pernanbuco', 
             'uf': 'PE', 
             "cep": "52190275"
 
@@ -113,12 +113,88 @@ db.empresas.insertMany(
             "rua": "2ª Travessa Córrego do Abdias",
             "bairro": "Linha do Tiro", 
             "cidade":"Recife", 
-            "estado": 'Recife', 
+            "estado": 'Pernanbuco', 
             'uf': 'PE', 
             "cep": "52131241"
 
         }
 
+    }, 
+
+    {
+
+        "_id" : 7, 
+        "CNPJ": "77830477000129", 
+        "Razão Social": "Mauriceia Alimentos", 
+        "descrição": "Nascemos em 1988, mas nossa história começou anos antes, numa pequena criação de aves em Nazaré da Mata, Pernambuco. Cada experiência, conhecimento e aptidão adquiridos nos levaram à busca por um espaço maior. E, em pouco tempo, tivemos que expandir para uma fábrica em Carpina, onde construímos uma fábrica de rações, granja de matrizes, incubatório e abatedouro. De lá pra cá, não paramos de crescer. Hoje, temos agroindústrias de larga produção em Pernambuco, Paraíba e Bahia.",
+        "endereço": {
+
+            "rua": "Rua Bernardo Vieira de Melo 19",
+            "bairro": "Centro", 
+            "cidade":"Nazaré da Mata", 
+            "estado": '`Pernanbuco`', 
+            'uf': 'PE', 
+            "cep": "55800970"
+
+        }
+
+    }, 
+
+    {
+
+        "_id" : 8, 
+        "CNPJ": "86346320000113", 
+        "Razão Social": "Alpargatas", 
+        "descrição": "We are a global company, founded and based in Brazil for over 115 years. We are committed to delight the world with amazing brands, that convey lightness and joy to the everyday lives of our consumers. We own Havaianas brand, world leader in open shoes, known for the iconic flip-flops that represent Brazilianness, comfort and style. We have 49.2% of Rothy’s a north-american sustainable footwear Brand.",
+        "endereço": {
+
+            "rua": "Rua Deputado João Teobaldo de Azevedo",
+            "bairro": "São Sebastião", 
+            "cidade":"Carpina", 
+            "estado": '`Pernanbuco`', 
+            'uf': 'PE', 
+            "cep": "55818520"
+
+        }
+
+    }, 
+
+    {
+
+        "_id" : 9, 
+        "CNPJ": "28935720000168", 
+        "Razão Social": "Bradesco", 
+        "descrição": "Valorizamos muito o que temos de “humano”. Nossa trajetória só obteve tanto sucesso ao longo destes mais de 80 anos graças às pessoas que construíram e constroem, dia após dia, o trabalho do Bradesco. Isso é vir em primeiro.",
+        "endereço": {
+
+            "rua": "Rua João Antonio Tavares",
+            "bairro": "Santo Antônio", 
+            "cidade":"Carpina", 
+            "estado": '`Pernanbuco`', 
+            'uf': 'PE', 
+            "cep": "55816355"
+
+        }
+
+    }, 
+
+    {
+
+        "_id" : 10, 
+        "CNPJ": "87314842000104", 
+        "Razão Social": "Confeitaria Colombo", 
+        "descrição": "Café centenário decorado com fabulosos espelhos, renomado por suas iguarias e doces confeitados.",
+        "endereço": {
+
+            "rua": "Rua João Moraes",
+            "bairro": "Paciência", 
+            "cidade":"Rio de Janeiro", 
+            "estado": '`Rio de Janeiro`', 
+            'uf': 'RJ', 
+            "cep": "23587620"
+
+        }
+
     }
 
-)
+])
