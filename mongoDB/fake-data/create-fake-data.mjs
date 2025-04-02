97a8ab056535f2ccf7fdab73bf57ffbfa686724c
Pedro Baptista <pedrolk2012@gmail.com>
2025-04-01 23:45:24 -0300

    feat: create faker functions

--- mongoDB/fake-data/create-fake-data.mjs
+++ mongoDB/fake-data/create-fake-data.mjs
@@ -10,7 +10,7 @@ if (!DATABASE_URL) {
 
 const client = new MongoClient(DATABASE_URL);
 
-async function populateVagas() {
+async function populateCompanies() {
   try {
     await client.connect();
     const db = client.db("gdi-project"); // Replace with your database name
@@ -51,4 +51,156 @@ async function populateVagas() {
   }
 }
 
-populateDatabase();
+async function createVagasForEmpresas() {
+  try {
+    await client.connect();
+    const db = client.db("gdi-project"); // Replace with your database name
+    const empresasCollection = db.collection("empresas");
+    const vagasCollection = db.collection("vagas");
+
+    const allCompanies = await empresasCollection.find().toArray();
+    const lastVaga = await vagasCollection
+      .find()
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray();
+    let nextId = lastVaga.length > 0 ? lastVaga[0]._id + 1 : 1;
+    let fakeVagas = [];
+
+    for (const company of allCompanies) {
+      const hasVagas = await vagasCollection.findOne({
+        empresa_id: company._id,
+      });
+      if (!hasVagas) {
+        for (let i = 0; i < 2; i++) {
+          fakeVagas.push({
+            _id: nextId++,
+            titulo: faker.person.jobTitle(),
+            descrição: faker.lorem.sentences(3),
+            empresa_id: company._id,
+            beneficios: faker.helpers.arrayElements(
+              [
+                "Plano de saúde e odontológico",
+                "Vale alimentação ou refeição",
+                "Horário flexível",
+                "Opção de trabalho remoto ou híbrido",
+                "Auxílio para cursos e certificações",
+                "Ambiente colaborativo e inovador",
+              ],
+              4
+            ),
+            requisitos: faker.helpers.arrayElements(
+              [
+                "Experiência prévia na área",
+                "Conhecimento técnico relevante",
+                "Habilidades de comunicação",
+                "Capacidade de trabalho em equipe",
+                "Familiaridade com ferramentas e softwares específicos",
+                "Disponibilidade para horários flexíveis",
+              ],
+              5
+            ),
+            tipo: faker.helpers.arrayElement(["CLT", "PJ", "Freelancer"]),
+            salário: faker.number.int({
+              min: 2000,
+              max: 10000,
+              precision: 0.01,
+            }),
+            localização: company.endereço.cidade,
+            data_publicação: faker.date.recent(),
+          });
+        }
+      }
+    }
+
+    if (fakeVagas.length > 0) {
+      const vagasResult = await vagasCollection.insertMany(fakeVagas);
+      console.log(`${vagasResult.insertedCount} fake vagas inserted.`);
+    } else {
+      console.log("No new fake vagas were needed.");
+    }
+  } catch (error) {
+    console.error("Error creating job vacancies:", error);
+  } finally {
+    await client.close();
+  }
+}
+
+async function createCandidatos() {
+  try {
+    await client.connect();
+    const db = client.db("gdi-project"); // Replace with your database name
+    const candidatosCollection = db.collection("candidatos");
+
+    let candidatos = [];
+
+    const lastCandidato = await db
+      .collection("candidatos")
+      .find()
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray();
+    let nextId = lastCandidato.length > 0 ? lastCandidato[0]._id + 1 : 1;
+    // Create 5 fake candidatos
+    for (let i = 0; i < 50; i++) {
+      candidatos.push({
+        _id: nextId + i + 1,
+        nome: faker.person.fullName(),
+        email: faker.internet.email(),
+        telefone: faker.phone.number(),
+        curriculo: `Curriculo ${faker.person.fullName()}.pdf`,
+        habilidades: [
+          faker.word.adjective(),
+          faker.word.adjective(),
+          faker.word.adjective(),
+          faker.word.adjective(),
+          faker.word.adjective(),
+        ],
+        experiência: [
+          {
+            empresa: faker.company.name(),
+            cargo: faker.person.jobTitle,
+            inicio: faker.date.past().toISOString().split("T")[0],
+            fim: faker.date.future().toISOString().split("T")[0],
+          },
+          {
+            empresa: faker.company.name(),
+            cargo: faker.person.jobTitle(),
+            inicio: faker.date.past().toISOString().split("T")[0],
+            fim: faker.date.future().toISOString().split("T")[0],
+          },
+        ],
+        formação: [
+          {
+            instituição: faker.company.name(),
+            curso: faker.person.jobType(),
+            tipo: faker.helpers.arrayElement([
+              "Bacharelado",
+              "Mestrado",
+              "Tecnólogo",
+            ]),
+            "data de conclusão": faker.date
+              .future()
+              .toISOString()
+              .split("T")[0],
+          },
+        ],
+        categoria: faker.helpers.arrayElement([
+          "Tecnologia",
+          "Engenharia",
+          "Cozinha",
+        ]),
+      });
+    }
+
+    const result = await candidatosCollection.insertMany(candidatos);
+    console.log(`${result.insertedCount} fake candidatos inserted.`);
+  } catch (error) {
+    console.error("Error creating candidatos:", error);
+  } finally {
+    await client.close();
+  }
+}
+
+// Call the function
+createCandidatos();
